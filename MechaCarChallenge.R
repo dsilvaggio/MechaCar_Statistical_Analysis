3e5c35f38f7afc5758bd5a23dd2616c106edfb17
Danielle Silvaggio <daniellesilvaggio@gmail.com>
2022-06-02 15:43:53 -0700

    Delete MechaCarChallenge.R

--- MechaCarChallenge.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-mecha_car_table <-read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-
-lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha_car_table)
-
-summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha_car_table))
-
